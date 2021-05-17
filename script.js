9f71f8a8b03a3512ee2ceba933090376bdec11b0
krowter <krishna.rowter@gmail.com>
2021-05-17 18:24:46 +0700

    add speech recognition script

---
+++ script.js
@@ -0,0 +1,49 @@
+const warning = document.getElementById("warning");
+const toggleButton = document.getElementById("toggle-button");
+const toggleLanguage = document.getElementById("toggle-language");
+
+let recognition = {},
+  isRecognizing;
+
+recognition = new webkitSpeechRecognition();
+
+const initialOptions = {
+  lang: "id-ID",
+  continuous: true,
+  interimResults: true,
+  onend: reset(),
+};
+
+Object.assign(recognition, initialOptions);
+
+recognition.onresult = function (event) {
+  for (var i = event.resultIndex; i < event.results.length; ++i) {
+    //only display the final transcript
+    textarea.value = [...event.results].map((result) => result[0].transcript);
+  }
+};
+
+toggleLanguage.onchange = function (event) {
+  const language = event.target.value;
+
+  recognition.lang = language;
+  toggleStartStop();
+};
+
+function reset() {
+  isRecognizing = false;
+  toggleButton.textContent = "Click to Speak";
+}
+
+function toggleStartStop() {
+  if (isRecognizing) {
+    toggleButton.classList.remove("is-active");
+    recognition.stop();
+    reset();
+  } else {
+    toggleButton.classList.add("is-active");
+    recognition.start();
+    isRecognizing = true;
+    toggleButton.textContent = "Click to Stop";
+  }
+}
